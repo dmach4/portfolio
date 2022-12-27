206ecabf18c131922672a76dde27c5f4f51b3db3
dmach4 <dmachfour@gmail.com>
2022-12-27 08:21:20 -0300

    mudei comando do calc js

--- js/calc.js
+++ js/calc.js
@@ -3,18 +3,18 @@ const telaResultado = document.querySelector('#result')
 let conta;
 
 function botao(value) {
-    conta = telaResultado.value += value
+  conta = telaResultado.value += value
 }
 function limpar() {
-  telaResultado.value =""
+  telaResultado.value ="";
 }
 function calc() {
   const resultado = eval(conta)
   telaResultado.value = resultado
 }
 function retirar() {
-    const palavraCompleta = telaResultado.value
-    const ultimaLetra = palavraCompleta.substring(0, telaResultado.value.length - 1)
-    console.log(ultimaLetra)
-    telaResultado.value = ultimaLetra
+  const palavraCompleta = telaResultado.value
+  const ultimaLetra = palavraCompleta.substring(0, telaResultado.value.length - 1)
+  console.log(ultimaLetra)
+  telaResultado.value = ultimaLetra
 }
