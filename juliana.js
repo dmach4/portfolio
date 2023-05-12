94a0828ec5227c6f7ff1d0f71ae165104bcaa611
dmach4 <dmachfour@gmail.com>
2023-05-12 19:33:43 -0300

    vibra 100

--- juliana.js
+++ juliana.js
@@ -3,7 +3,7 @@ function abrirMenu(){
     if (Item.style.display == "block")
     {
         Item.style.display ="none";
-        navigator.vibrate(1000);
+        navigator.vibrate(100);
     }
     else
     {
