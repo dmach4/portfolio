12574c835bdce5f196e2e982d4ead464c19df927
dmach4 <dmachfour@gmail.com>
2023-05-30 08:33:00 -0300

    02

--- juliana.js
+++ juliana.js
@@ -2,7 +2,7 @@ function abrirMenu(){
     if (Item.style.display == "block")
     {
         Item.style.display ="none";
-        navigator.vibrate(1000);
+        navigator.vibrate(500);
     }
     else
     {
